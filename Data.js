f5030a838ef88422eb92661f0cff8c15d2e4d5e6
Pedro Henrique Camacho <pedrohcamacho@hotmail.com>
2023-12-28 12:32:25 -0300

    criei um arquivo para preparar a lista para receber dados da API

---
+++ Data.js
@@ -0,0 +1,10 @@
+export default  {
+  "Book": "Luke",
+  "Chapter": "1",
+  "Output": "1 As a number of attempts have been made to put together in order an account of those events which took place among us, 2 As they were handed down to us by those who saw them from the first and were preachers of the word, 3 It seemed good to me, having made observation, with great care, of the direction of events in their order, to put the facts in writing for you, most noble Theophilus; 4 So that you might have certain knowledge of those things about which you were given teaching. 5 In the days of Herod, king of Judaea, there was a certain priest, by name Zacharias, of the order of Abijah; and he had a wife of the family of Aaron, and her name was Elisabeth. ",
+  "VerseFrom": "1",
+  "VerseTo": "5",
+}
+
+   
+  
