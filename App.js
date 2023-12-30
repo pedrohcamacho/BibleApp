66f2e4d60528e3a0a6243ab98f1f0999ec3a1f91
Pedro Henrique Camacho <pedrohcamacho@hotmail.com>
2023-12-29 22:02:54 -0300

    adicionando nova fonte italica

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import {Routes} from './src/routes';
 import {Loading} from './src/components/Loading';
-import { useFonts, Lato_700Bold, Lato_900Black, Lato_400Regular } from '@expo-google-fonts/lato';
+import { useFonts, Lato_700Bold, Lato_900Black, Lato_400Regular, Lato_400Regular_Italic } from '@expo-google-fonts/lato';
 export default function App() {
 
   let [fontsLoaded, fontError] = useFonts({
-    Lato_700Bold, Lato_900Black, Lato_400Regular
+    Lato_700Bold, Lato_900Black, Lato_400Regular,Lato_400Regular_Italic
   });
 
   if (!fontsLoaded && !fontError) {
